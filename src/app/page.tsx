30f484fd840c51a1eb2aa135a96844d28c8602b9
ThawTunZan <thawtunzan@gmail.com>
2026-04-02 19:14:31 +0800

    add chatbot UI

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Chatbot from "@/app/chatbot/page";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+
+      <Chatbot></Chatbot>
+    </main>
+  );
+}
